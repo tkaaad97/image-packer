953c3313f49c71cd7411c2f6f61e320dac6fd23e
tkaaad97 <tkaaad97@gmail.com>
2022-02-28 21:07:10 +0900

    change naming

--- src/packer.rs
+++ src/packer.rs
@@ -13,7 +13,7 @@ pub struct Packer {
 #[derive(Debug)]
 struct Packed {
     pub layouts: Vec<Layout>,
-    pub regions: Regions,
+    pub spaces: Spaces,
 }
 
 #[derive(Debug)]
@@ -30,8 +30,8 @@ pub struct Rect {
 }
 
 #[derive(Debug)]
-pub(crate) struct Regions {
-    regions: BTreeMap<usize, BTreeMap<usize, Vec<Rect>>>
+pub(crate) struct Spaces {
+    spaces: BTreeMap<usize, BTreeMap<usize, Vec<Rect>>>
 }
 
 impl Rect {
@@ -97,85 +97,85 @@ impl Rect {
     }
 }
 
-impl Regions {
-    pub fn new(size: [usize; 2]) -> Regions {
+impl Spaces {
+    pub fn new(size: [usize; 2]) -> Spaces {
         let area = size[0] * size[1];
         let rect = Rect {
             size,
             position: [0, 0],
         };
-        return Regions { regions: BTreeMap::from([(area, BTreeMap::from([(size[0], Vec::from([rect]))]))]) };
+        return Spaces { spaces: BTreeMap::from([(area, BTreeMap::from([(size[0], Vec::from([rect]))]))]) };
     }
 
     pub fn find_space(&self, size: [usize; 2]) -> Option<Rect> {
         let area = size[0] * size[1];
-        for (region_area, regions_equal_area) in self.regions.range((Included(area), Unbounded)) {
-            if let Some((_, found_regions)) = regions_equal_area
+        for (space_area, spaces_equal_area) in self.spaces.range((Included(area), Unbounded)) {
+            if let Some((_, found_spaces)) = spaces_equal_area
                     .range((Included(size[0]), Unbounded))
-                    .find(|(region_width, regions_equal_width)| !regions_equal_width.is_empty() && (**region_width >= size[0]) && (*region_area >= (size[1] * (**region_width)))) {
-                return Some(Rect{ size:found_regions[0].size, position: found_regions[0].position});
+                    .find(|(space_width, spaces_equal_width)| !spaces_equal_width.is_empty() && (**space_width >= size[0]) && (*space_area >= (size[1] * (**space_width)))) {
+                return Some(Rect{ size:found_spaces[0].size, position: found_spaces[0].position});
             }
         }
         return None;
     }
 
     pub fn exclude(&mut self, other: &Rect) {
-        let mut divided_regions: Vec<Rect> = Vec::new();
-        for (_, regions_equal_area) in self.regions.iter_mut() {
-            for (_, regions_equal_width) in regions_equal_area.into_iter() {
+        let mut divided_spaces: Vec<Rect> = Vec::new();
+        for (_, spaces_equal_area) in self.spaces.iter_mut() {
+            for (_, spaces_equal_width) in spaces_equal_area.into_iter() {
                 let mut remove_indices: Vec<usize> = Vec::new();
-                for (i, region) in regions_equal_width.into_iter().enumerate() {
-                    if region.has_intersection(other) {
+                for (i, space) in spaces_equal_width.into_iter().enumerate() {
+                    if space.has_intersection(other) {
                         remove_indices.push(i);
-                        divided_regions.append(&mut region.divide(&Rect{size: other.size, position: other.position}));
+                        divided_spaces.append(&mut space.divide(&Rect{size: other.size, position: other.position}));
                     }
                 }
 
                 // remove reversely not to change other indices
                 for i in remove_indices.iter().rev() {
-                    regions_equal_width.remove(*i);
+                    spaces_equal_width.remove(*i);
                 }
             }
         }
 
         // remove empty
-        for (_, regions_equal_area) in self.regions.iter_mut() {
-            regions_equal_area.retain(|_, a|!a.is_empty());
+        for (_, spaces_equal_area) in self.spaces.iter_mut() {
+            spaces_equal_area.retain(|_, a|!a.is_empty());
         }
-        self.regions.retain(|_, a|!a.is_empty());
+        self.spaces.retain(|_, a|!a.is_empty());
 
-        // sort new divided regions by area
-        divided_regions.sort_by(|a, b|(b.size[0] * b.size[1]).cmp(&(a.size[0] * a.size[1])));
+        // sort new divided spaces by area
+        divided_spaces.sort_by(|a, b|(b.size[0] * b.size[1]).cmp(&(a.size[0] * a.size[1])));
 
-        // add new regions
-        for region in divided_regions {
-            self.add(region);
+        // add new spaces
+        for space in divided_spaces {
+            self.add(space);
         }
     }
 
-    pub fn add(&mut self, new_region: Rect) {
-        let area = new_region.size[0] * new_region.size[1];
-        let width = new_region.size[0];
-        for (_, regions_equal_area) in self.regions.range((Included(area), Unbounded)) {
-            for (_, regions_equal_width) in regions_equal_area.range((Included(width), Unbounded)) {
-                for a in regions_equal_width {
-                    if a.include(&new_region) {
-                        // other region cover new one
+    pub fn add(&mut self, new_space: Rect) {
+        let area = new_space.size[0] * new_space.size[1];
+        let width = new_space.size[0];
+        for (_, spaces_equal_area) in self.spaces.range((Included(area), Unbounded)) {
+            for (_, spaces_equal_width) in spaces_equal_area.range((Included(width), Unbounded)) {
+                for a in spaces_equal_width {
+                    if a.include(&new_space) {
+                        // other space cover new one
                         return;
                     }
                 }
             }
         }
 
-        if let Some(regions_equal_area) = self.regions.get_mut(&area) {
-            if let Some(regions_equal_width) = regions_equal_area.get_mut(&width) {
-                regions_equal_width.push(new_region);
+        if let Some(spaces_equal_area) = self.spaces.get_mut(&area) {
+            if let Some(spaces_equal_width) = spaces_equal_area.get_mut(&width) {
+                spaces_equal_width.push(new_space);
             } else {
-                regions_equal_area.insert(width, vec![new_region]);
+                spaces_equal_area.insert(width, vec![new_space]);
             }
         } else {
-            self.regions.insert(area, BTreeMap::from([
-                (width, vec![new_region])
+            self.spaces.insert(area, BTreeMap::from([
+                (width, vec![new_space])
             ]));
         }
     }
@@ -183,7 +183,7 @@ impl Regions {
 
 impl Packed {
     pub fn new(texture_size: [usize; 2]) -> Packed {
-        return Packed { layouts: Vec::new(), regions: Regions::new(texture_size) };
+        return Packed { layouts: Vec::new(), spaces: Spaces::new(texture_size) };
     }
 }
 
@@ -227,18 +227,18 @@ impl Packer {
         (index, size): (usize, &[usize; 2]),
     ) -> bool {
         let size_with_spacing = [std::cmp::min(size[0] + self.spacing, self.texture_size[0]), std::cmp::min(size[1] + self.spacing, self.texture_size[1])];
-        if let Some(space) = packed.regions.find_space(size_with_spacing) {
+        if let Some(space) = packed.spaces.find_space(size_with_spacing) {
             let layout = Layout{ index, position: space.position, rotated: false };
             packed.layouts.push(layout);
-            packed.regions.exclude(&Rect{ position: space.position, size: size_with_spacing });
+            packed.spaces.exclude(&Rect{ position: space.position, size: size_with_spacing });
             return true;
         }
         if self.enable_rotate && size[1] <= self.texture_size[0] && size[0] <= self.texture_size[1] {
             let rotated_size = [std::cmp::min(size[1] + self.spacing, self.texture_size[0]), std::cmp::min(size[0] + self.spacing, self.texture_size[1])];
-            if let Some(space) = packed.regions.find_space(rotated_size) {
+            if let Some(space) = packed.spaces.find_space(rotated_size) {
                 let layout = Layout{ index, position: space.position, rotated: true };
                 packed.layouts.push(layout);
-                packed.regions.exclude(&Rect{ position: space.position, size: rotated_size });
+                packed.spaces.exclude(&Rect{ position: space.position, size: rotated_size });
                 return true;
             }
         }
